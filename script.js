ad5e7376b45c8bb9777e84b10d29e72cd33ef4dd
Wittner Andrews <wittnerandrews@gmail.com>
2025-02-01 05:03:32 -0600

    experimenting making the boxes

--- script.js
+++ script.js
@@ -17,6 +17,23 @@ const box14 = document.createElement("div")
 const box15 = document.createElement("div")
 const box16 = document.createElement("div")
 
+box1.setAttribute("class", "gridbox");
+box2.setAttribute("class", "gridbox");
+box3.setAttribute("class", "gridbox");
+box4.setAttribute("class", "gridbox");
+box5.setAttribute("class", "gridbox");
+box6.setAttribute("class", "gridbox");
+box7.setAttribute("class", "gridbox");
+box8.setAttribute("class", "gridbox");
+box9.setAttribute("class", "gridbox");
+box10.setAttribute("class", "gridbox");
+box11.setAttribute("class", "gridbox");
+box12.setAttribute("class", "gridbox");
+box13.setAttribute("class", "gridbox");
+box14.setAttribute("class", "gridbox");
+box15.setAttribute("class", "gridbox");
+box16.setAttribute("class", "gridbox");
+
 container.appendChild(box1);
 container.appendChild(box2);
 container.appendChild(box3);
@@ -33,4 +50,6 @@ container.appendChild(box13);
 container.appendChild(box14);
 container.appendChild(box15);
 container.appendChild(box16);
+
+console.log(container);
 
