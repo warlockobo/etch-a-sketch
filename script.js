5bbe41c66a6f40dd74b6e07272dcf5dd317719cb
Wittner Andrews <wittnerandrews@gmail.com>
2025-02-01 04:39:10 -0600

    created all the boxes and container

---
+++ script.js
@@ -0,0 +1,36 @@
+const container = document.querySelector(".container");
+
+const box1 = document.createElement("div")
+const box2 = document.createElement("div")
+const box3 = document.createElement("div")
+const box4 = document.createElement("div")
+const box5 = document.createElement("div")
+const box6 = document.createElement("div")
+const box7 = document.createElement("div")
+const box8 = document.createElement("div")
+const box9 = document.createElement("div")
+const box10 = document.createElement("div")
+const box11 = document.createElement("div")
+const box12 = document.createElement("div")
+const box13 = document.createElement("div")
+const box14 = document.createElement("div")
+const box15 = document.createElement("div")
+const box16 = document.createElement("div")
+
+container.appendChild(box1);
+container.appendChild(box2);
+container.appendChild(box3);
+container.appendChild(box4);
+container.appendChild(box5);
+container.appendChild(box6);
+container.appendChild(box7);
+container.appendChild(box8);
+container.appendChild(box9);
+container.appendChild(box10);
+container.appendChild(box11);
+container.appendChild(box12);
+container.appendChild(box13);
+container.appendChild(box14);
+container.appendChild(box15);
+container.appendChild(box16);
+
