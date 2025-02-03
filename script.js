b1ac914589c6611cd42af768d0d69fc0dd240d99
Wittner Andrews <wittnerandrews@gmail.com>
2025-02-02 21:36:58 -0600

    boxes appear and change color

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
 const container = document.querySelector(".container");
 
-function gridCreate (size) {
-    const boxSize = 
+function colorRandomization() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
 }
+
+function gridCreate (size) {
+    container.textContent = '';
+    const boxSize = 720 / size;
+
+    for (let i = 0; i < size * size; i++) {
+        const gridBox = document.createElement("div");
+        gridBox.classList.add("gridBox");
+        gridBox.style.width = `${boxSize}px`;
+        gridBox.style.height = `${boxSize}px`;
+        container.appendChild(gridBox);
+    }
+
+    const boxes = document.querySelectorAll(".gridBox");
+
+    boxes.forEach((gridBox) => {
+      gridBox.addEventListener("mouseover", () => {
+        const randomColor = colorRandomization();
+        gridBox.style.backgroundColor = randomColor;
+  
+        let currentOpacity = parseFloat(gridBox.style.opacity);
+        if (currentOpacity < 1) {
+          gridBox.style.opacity = (currentOpacity + 0.1).toString();
+        }
+      });
+    });
+  }
+
+gridCreate(16);
