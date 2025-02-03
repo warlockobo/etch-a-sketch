13c7201a8c999eec129c39119519a9efc449e00a
Wittner Andrews <wittnerandrews@gmail.com>
2025-02-02 20:36:28 -0600

    rewriting & creating boxes

--- script.js
+++ script.js
@@ -1,54 +1,5 @@
 const container = document.querySelector(".container");
 
-const box1 = document.createElement("div")
-const box2 = document.createElement("div")
-const box3 = document.createElement("div")
-const box4 = document.createElement("div")
-const box5 = document.createElement("div")
-const box6 = document.createElement("div")
-const box7 = document.createElement("div")
-const box8 = document.createElement("div")
-const box9 = document.createElement("div")
-const box10 = document.createElement("div")
-const box11 = document.createElement("div")
-const box12 = document.createElement("div")
-const box13 = document.createElement("div")
-const box14 = document.createElement("div")
-const box15 = document.createElement("div")
-const box16 = document.createElement("div")
-
-box1.setAttribute("class", "gridbox");
-box2.setAttribute("class", "gridbox");
-box3.setAttribute("class", "gridbox");
-box4.setAttribute("class", "gridbox");
-box5.setAttribute("class", "gridbox");
-box6.setAttribute("class", "gridbox");
-box7.setAttribute("class", "gridbox");
-box8.setAttribute("class", "gridbox");
-box9.setAttribute("class", "gridbox");
-box10.setAttribute("class", "gridbox");
-box11.setAttribute("class", "gridbox");
-box12.setAttribute("class", "gridbox");
-box13.setAttribute("class", "gridbox");
-box14.setAttribute("class", "gridbox");
-box15.setAttribute("class", "gridbox");
-box16.setAttribute("class", "gridbox");
-
-container.appendChild(box1);
-container.appendChild(box2);
-container.appendChild(box3);
-container.appendChild(box4);
-container.appendChild(box5);
-container.appendChild(box6);
-container.appendChild(box7);
-container.appendChild(box8);
-container.appendChild(box9);
-container.appendChild(box10);
-container.appendChild(box11);
-container.appendChild(box12);
-container.appendChild(box13);
-container.appendChild(box14);
-container.appendChild(box15);
-container.appendChild(box16);
-
-console.log(container);
+function gridCreate (size) {
+    const boxSize = 
+}
